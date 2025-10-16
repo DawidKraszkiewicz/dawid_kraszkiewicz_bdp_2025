f28a212bc2236b75f29a701a423a4f6970ae47c9
Dawid Tomasz Kraszkiewicz <39964989+DawidKraszkiewicz@users.noreply.github.com>
2025-10-16 12:46:44 +0200

    Create tables for employee management in ksiegowosc
    This SQL script creates four tables in the ksiegowosc schema: pracownicy, godziny, pensja, and premia, each with their respective columns and constraints.

---
+++ 01_setup/zad3.sql
@@ -0,0 +1,29 @@
+
+CREATE TABLE ksiegowosc.pracownicy (
+    id_pracownika SERIAL PRIMARY KEY,
+    imie VARCHAR(30) NOT NULL,
+    nazwisko VARCHAR(40) NOT NULL,
+    adres VARCHAR(100),
+    telefon VARCHAR(15)
+) COMMENT = 'tabela z informacjami o pracownikach';
+
+CREATE TABLE ksiegowosc.godziny (
+    id_godziny SERIAL PRIMARY KEY,
+    data DATE NOT NULL,
+    liczba_godzin NUMERIC(5,2) NOT NULL,
+    id_pracownika INT REFERENCES ksiegowosc.pracownicy(id_pracownika)
+) COMMENT = 'tabela z godzinami przepracowanymi przez pracownikow';
+
+
+CREATE TABLE ksiegowosc.pensja (
+    id_pensji SERIAL PRIMARY KEY,
+    stanowisko VARCHAR(50) NOT NULL,
+    kwota NUMERIC(10,2) NOT NULL
+) COMMENT = 'tabela z informacjami o pensjach' ;
+
+
+CREATE TABLE ksiegowosc.premia (
+    id_premii SERIAL PRIMARY KEY,
+    rodzaj VARCHAR(50),
+    kwota NUMERIC(10,2)
+) COMMENT = 'tabela z informacjami o premiach';
